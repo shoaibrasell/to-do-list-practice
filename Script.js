f226aa02e51faa237b0e6143a72ef459edd8ce4e
Shoaib Ullah Rasel <shoaibullahrasel@gmail.com>
2023-05-13 00:10:49 +0600

    Changes commit

--- Script.js
+++ Script.js
@@ -23,7 +23,7 @@ let addTask = function (event) {
     let listItem = createTask(newTask.value);
     todoUl.appendChild(listItem);
     newTask.value = "";
-    //bind list
+    
     bindInCompleteItems(listItem, completeTask);
 }
 
